9137864eee0f9b129a2de8ce88400cb8228eb81f
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-09 15:33:54 +0530

    Update Device.php

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Device extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'brand_id',
+        'model_number',
+        'series',
+        'name',
+        'description'
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
