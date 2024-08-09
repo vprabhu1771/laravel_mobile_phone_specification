7830b9eb043dc0e102aebfe0afa2b8ce5ce36dec
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-09 15:48:14 +0530

    Create Feature.php

---
+++ app/Models/Feature.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'device_id',
+        'feature',
+    ];
+
+    /**
+     * Get the device that owns the feature.
+     */
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
+}
