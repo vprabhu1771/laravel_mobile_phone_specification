d9c4d755ca92f423a0a31c13cc18fd58bb0e50ff
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-11 09:18:00 +0530

    home partial setup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        return view('frontend/home');
+    }
 }
---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,11 @@
+@extends('frontend/layout/app')
+
+@section('title')
+
+@endsection
+
+@section('content')
+
+home
+
+@endsection
