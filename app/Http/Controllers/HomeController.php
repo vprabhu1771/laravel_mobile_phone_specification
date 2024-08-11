92d073b1be9c4be0ab44e3d85209d9c0f6ea3209
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-11 13:30:09 +0530

    added search using category, search term

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+use App\Models\Brand;
+use App\Models\Device;
+
 class HomeController extends Controller
 {
     //
     public function index(Request $request)
     {
-        return view('frontend/home');
+        $categories= Category::all();
+
+        $query = Device::query();
+
+        // Apply filters based on the request
+        if ($request->has('category') && $request->category != 'All') {
+            $query->where('category_id', $request->category);
+        }
+
+        if ($request->has('search')) {
+            $searchTerm = $request->input('search');
+            $query->where('name', 'like', "%{$searchTerm}%");
+        }
+
+        $data = [
+            'product' => $query->get(),
+            'categories' => $categories
+        ];
+
+        // dd($data);
+
+        return view('frontend/home', $data);
     }
+
+    
 }
