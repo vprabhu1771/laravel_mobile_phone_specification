72599dd455fa734a0d4ac0bdbca144ea028b197c
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-11 13:17:10 +0530

    header nav bar ui added

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,1 +1,36 @@
-<h1>header</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Small Shop</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            
+            @guest
+            <li class="nav-item">
+                <a class="nav-link" href="/login">Login / Register</a>
+            </li>
+            @endguest
+            
+            @if (Auth::check())
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->name }} <!-- Display the user's name -->
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('cart.index') }}">Cart</a></li>
+                        <li><a class="dropdown-item" href="{{ route('order.index') }}">Order</a></li>
+                        <li><a class="dropdown-item" href="{{ route('home.profile') }}">Profile</a></li>
+                        <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
+
+                        
+                    </ul>
+                </li>
+            @endif
+
+        </ul>
+    </div>
+</nav>
