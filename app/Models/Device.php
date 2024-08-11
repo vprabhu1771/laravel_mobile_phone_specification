2032de31e1a4eb94ee5e25bf014d41173de123ad
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-11 13:18:10 +0530

    Update Device.php

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -5,23 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Facades\Storage;
+
 class Device extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'category_id',
         'brand_id',
         'model_number',
         'series',
         'name',
-        'description'
+        'description',
+        'image_path'
     ];
+
+    public function GetImagePath()
+    {
+        return env('DOMAIN_URL') . Storage::url($this->image_path);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
     public function brand()
     {
         return $this->belongsTo(Brand::class);
     }
-
     
     /**
      * Get the specifications for the device.
