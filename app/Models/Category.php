b9850edb7cd9d9410a17b3575dc8fc3ba95e44d1
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-11 13:18:24 +0530

    device relationship added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+    
+    public function devices()
+    {
+        return $this->hasMany(Device::class);
+    }
 }
