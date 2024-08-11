b4f2002e475060a5359bb407ae0c9a533f3ecf8b
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-11 12:04:42 +0530

    search ui added

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -8,4 +8,16 @@
 
 home
 
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <!-- Search Bar -->
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-button">
+                <button class="btn btn-primary" type="button" id="search-button">Search</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
